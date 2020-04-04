301b0c159b17028db828c840e2902c0649f1984e
Matt Fleming <matthewfleming8@gmail.com>
2020-04-04 01:53:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,4 +26,20 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
   
